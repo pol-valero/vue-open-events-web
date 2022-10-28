d7135f8e82a7927e6219cfa6aef11248c8466192
angel.garcia <angel.garcia@students.salle.url.edu>
2022-10-28 17:46:22 +0200

    Preparat entorn de treball per timeline

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import { RouterLink, RouterView } from "vue-router";
   <header>
     <nav>
       <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/timeline">Timeline</RouterLink>
     </nav>
   </header>
   <main>
---
+++ src/views/Timeline.vue
@@ -0,0 +1,3 @@
+<template>
+holaa
+</template>
