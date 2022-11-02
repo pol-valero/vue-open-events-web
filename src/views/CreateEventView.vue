32bc942ad90e54398480691a61151b3dcaef9d9b
Pol Valero <polvalero@iMac-5K-de-Pol.lan>
2022-11-02 19:13:06 +0100

    S'ha creat la estructura inicial de la vista de createEvent. S'ha linkejat la vista en el router.

---
+++ src/views/CreateEventView.vue
@@ -0,0 +1,30 @@
+<style>
+
+/*aside {
+  display: none;
+}*/
+
+#create-event-main{
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  display: flex;
+}
+
+#title-container {
+  margin: 20px;
+  text-align: center;
+  font-size: 15px;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+}
+
+
+</style>
+
+<template>
+  <div class="create-event-main">
+    <section id = "title-container">
+      <h2>Create event</h2>
+    </section>
+  </div>
+</template>
