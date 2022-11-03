631fded503725d453328994388b971d875357071
Pol Valero <polvalero@iMac-5K-de-Pol.lan>
2022-11-03 14:21:01 +0100

    S'ha posat titol als diferents camps del formulari i s'ha fet que els camps mostrin un text per defecte

--- src/views/CreateEventView.vue
+++ src/views/CreateEventView.vue
@@ -33,17 +33,41 @@
 
 .dual-form{
   display: flex;
-  flex-direction: row;
+  flex-flow: row wrap;
 }
 
 .dual-form div {
   flex-grow: 1;
 }
 
+.textarea {
+  resize: none
+}
+
+.button-container {
+  margin: 30px;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-evenly;
+}
+
+.create-event-button {
+  margin: 5px;
+  background-color: black;
+  color: aliceblue;
+  padding: 10px;
+}
+
+.cancel-button{
+  margin: 5px;
+  padding: 10px;
+}
 
 </style>
 
+
 <template>
+
   <div id="create-event-main">
     
     <section id = "title-container">
@@ -53,52 +77,53 @@
     <section id="forms-container">
       <div class="single-form">
         <h4 class="form-title">Title</h4>  
-        <input type="text" />
+        <input type="text"  placeholder="Ex.- House BBQ"/>
       </div>
 
       <div class="single-form">
-        <h4 class="form-title">Title</h4>  
-        <input type="text" />
+        <h4 class="form-title">Description</h4>  
+        <textarea class="textarea" name="Text1" cols="40" rows="5"  placeholder="Ex.- This event is fun!"></textarea>
       </div>
       
       <div class="dual-form">
         <div class="single-form">
-          <h4 class="form-title">Title</h4>  
-          <input type="text" />
+          <h4 class="form-title">Start date</h4>  
+          <input type="date" />
         </div>
         <div class="single-form">
-          <h4 class="form-title">Title</h4>  
-          <input type="text" />
+          <h4 class="form-title">End date</h4>  
+          <input type="date" />
         </div>
       </div>
 
       <div class="single-form">
-        <h4 class="form-title">Title</h4>  
-        <input type="text" />
+        <h4 class="form-title">Location</h4>  
+        <input type="text"  placeholder="Ex.- Teruel"/>
       </div>
 
       <div class="single-form">
-        <h4 class="form-title">Title</h4>  
-        <input type="text" />
+        <h4 class="form-title">Image</h4>  
+        <input type="url" placeholder="Ex.- www.url-of-current-img.com"  />
       </div>
 
       <div class="dual-form">
         <div class="single-form">
-          <h4 class="form-title">Title</h4>  
-          <input type="text" />
+          <h4 class="form-title">Type</h4>  
+          <input type="text" placeholder="Ex.- Sports" />
         </div>
         <div class="single-form">
-          <h4 class="form-title">Title</h4>  
-          <input type="text" />
+          <h4 class="form-title">Capacity</h4>  
+          <input type="number" placeholder="Ex.- 50"  />
         </div>
       </div>
-      
+
+      <div class="button-container">
+        <button class="create-event-button">CREATE EVENT</button>
+        <button class="cancel-button">CANCEL</button>
+      </div>
       
     </section>
 
   </div>
     
-      
-  
-
 </template>
