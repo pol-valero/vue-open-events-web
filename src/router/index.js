c9df48ca0e2143d5992a16fcba2cefe09b5e60ec
Pol Valero <polvalero@iMac-5K-de-Pol.lan>
2022-11-05 14:27:12 +0100

    S'ha afegit el path de la vista EditEvent al index.js

--- src/router/index.js
+++ src/router/index.js
@@ -49,6 +49,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/CreateEventView.vue"),
     },
+    {
+      path: "/editEvent",
+      name: "editEvent",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/EditEventView.vue"),
+    },
   ],
 });
 
