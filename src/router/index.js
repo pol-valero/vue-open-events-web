33e33698d4d78cb1d056b933c15c40d82de754e1
Pol Valero <polvalero@iMac-5K-de-Pol.lan>
2022-11-06 11:01:46 +0100

    S'ha creat la estructura inicial de la vista de Event Details. S'ha afegit una imatge mes allargada per a poder ser utilitzada com a background. Tambe s'ha afegit la vista en el index.js

--- src/router/index.js
+++ src/router/index.js
@@ -65,6 +65,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/CreateEventView.vue"),
     },
+    {
+      path: "/eventDetails",
+      name: "eventDetails",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/EventDetailsView.vue"),
+    },
   ],
 });
 
---
+++ src/views/EventDetailsView.vue
@@ -0,0 +1,71 @@
+<style>
+
+/*aside {
+  display: none;
+}*/
+
+#event-details-main{
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  display: flex;
+}
+
+#title-container {
+  background-image: url("../assets/event-details-background.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 30%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+#text-title {
+    padding-left: 80px;
+    font-size: 20px;
+    width: 100%
+}
+
+#button-title {
+    display: flex;
+    flex-direction: row;
+    justify-content: end;
+    width: 100%;
+    padding-right: 100px;
+}
+
+#button-title button {
+    height: 50px;
+    width: 120px;
+}
+
+
+</style>
+
+
+<template>
+
+  <div id="event-details-main">
+    
+    <section id = "title-container">
+       
+        <div id ="text-title">
+            <h2>Create event</h2>
+        </div>
+      
+        <div id = "button-title">
+            <button>Share</button>
+        </div>
+        
+    </section>
+
+    <section id="details-container">
+      
+      
+    </section>
+
+  </div>
+    
+</template>
