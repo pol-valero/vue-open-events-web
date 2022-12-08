f35504274c3e33b0070e9abc3e12dfa5cf0e1f5c
angel.garcia <angel.garcia@students.salle.url.edu>
2022-12-08 18:47:09 +0100

    Finished chat logic

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -36,13 +36,14 @@
         if(response.length > 0) {
           this.userFriends = res;
           console.log(this.userFriends);
-
+          // We show the first friend's chat by default
+          this.showChat(this.userFriends[0]);
         } else {
           alert("You have no friends yet, try to add some!");
         }
       });
     },
-
+    
     methods: {
       showChat(friendChatting){
         // We first change the user that we are chatting with
@@ -102,14 +103,17 @@
 
         .then(res => res.json())
         .then(res=> {
-          let response = JSON.stringify(res);
-          if(response.length > 0) {
-            console.log(res);
-            this.messages.push(res);
-          } else {
-            alert("You have no messages with this user yet... Try to send one!");
-          }
-        });
+          console.log(res);
+          console.log("------------------");
+          console.log(res.user_id_send);
+          console.log(this.userID);
+
+          console.log(res.user_id_send == this.userID);
+          console.log("------------------");
+
+          this.messages.push(res);
+        })
+        .catch(error => alert("The message couldn't be sent! Try again later!"));
       }
     }
   }
@@ -126,7 +130,7 @@
               <img
                 class="little-img"
                 v-bind:src="friend.image"
-                alt="logo"
+                alt="friend image"
               />
               <h5 class="name-padding">{{ friend.name + " " + friend.last_name }}</h5>
             </div>
@@ -137,14 +141,14 @@
 
     <section id="container-chat-interactive">
       <div id="container-chat-person-selected">
-        <img class="little-img" v-bind:src="userChatting.image" alt="logo" />
+        <img class="little-img" v-bind:src="userChatting.image" alt="friend image" />
         <RouterLink to="/profile" id="friend-profile-btn">
           <h5 id="name-big-padding"> {{ userChatting.name + " " + userChatting.surname }}</h5>
         </RouterLink>
       </div>
 
       <div v-for="message in messages" v-bind:key="message.id" id="container-full-chat">
-          <div v-if="(message.user_id_send === userID)"  class="my-message">
+          <div v-if="(message.user_id_send == userID)"  class="my-message">
           <h4 class="message">{{ message.content }}</h4>
           <h6>{{ message.timeStamp.split("T")[0] + " / " + (message.timeStamp.split("T")[1]).substring(0, 5) }}</h6>
         </div> 
